2666bd9b968924692c44b2b3a782d45d9622d11c
Ludvig Vigerust <l.vigerust@gmail.com>
2023-05-09 02:33:37 +0200

    init

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Blaze Blog'
+export const description = 'SvelteKit blog for drunk authors'
+export const url = dev ? 'http://localhost:5173/' : 'http://stinkmeaner.com/'
